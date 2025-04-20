ba692f6f72c2540f3a1cbbb048bd582de6adddb6
Slayer of Madness <49862137+slayerofmadness@users.noreply.github.com>
2025-04-20 21:03:44 +0200

    Update custom_card.js

--- custom_components/kachelmannwetter/static/custom_card.js
+++ custom_components/kachelmannwetter/static/custom_card.js
@@ -1,1 +1,37 @@
-# DUMMY CONTENT
+class KachelmannWetterCard extends HTMLElement {
+  set hass(hass) {
+    const entityTemp = this.config.entity_temperature;
+    const entityCond = this.config.entity_condition;
+    const entityRain = this.config.entity_rain;
+
+    const temp = hass.states[entityTemp].state;
+    const cond = hass.states[entityCond].state;
+    const rain = hass.states[entityRain].state;
+
+    const showRain = parseInt(rain) > 30;
+
+    this.innerHTML = `
+      <ha-card header="Kachelmannwetter">
+        <div style="display:flex; align-items:center; justify-content:space-between; padding:1em">
+          <div>
+            <div style="font-size:2em;font-weight:bold">${temp} °C</div>
+            <div>Zustand: ${cond}</div>
+            <div>Regen: ${rain} %</div>
+          </div>
+          <div style="display:flex; align-items:center">
+            <img src="/local/community/kachelmannwetter/logo.png" style="height:48px;opacity:0.7; margin-right:0.5em"/>
+            ${showRain ? '<img src="/local/community/kachelmannwetter/rain.gif" style="height:64px;border-radius:8px"/>' : ''}
+          </div>
+        </div>
+      </ha-card>
+    `;
+  }
+
+  setConfig(config) {
+    this.config = config;
+  }
+
+  getCardSize() {
+    return 3;
+  }
+}
