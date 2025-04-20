d6b352b12bcef366249a6729bb7235529f5f1676
Slayer of Madness <49862137+slayerofmadness@users.noreply.github.com>
2025-04-20 21:01:37 +0200

    Update config_flow.py

--- custom_components/kachelmannwetter/config_flow.py
+++ custom_components/kachelmannwetter/config_flow.py
@@ -1,1 +1,21 @@
-# DUMMY CONTENT
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
+from .const import DOMAIN, CONF_API_KEY, CONF_LOCATION_NAME
+
+class KachelmannConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_LOCATION_NAME], data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_API_KEY): str,
+                vol.Required(CONF_LOCATION_NAME): str,
+                vol.Required(CONF_LATITUDE): float,
+                vol.Required(CONF_LONGITUDE): float
+            })
+        )
